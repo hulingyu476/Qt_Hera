7a621b2d7f05f33a7034a2fd20045153629799e3
silas <hulingyu476@126.com>
2020-09-28 16:09:57 +0800

    add LogMessage
    add LogMessage

--- MyQJson/main.cpp
+++ MyQJson/main.cpp
@@ -2,9 +2,76 @@
 
 #include <QApplication>
 
+#include <QFile>
+#include <stdio.h>
+#include <stdlib.h>
+#include <QtMsgHandler>
+#include <QTextStream>
+#include <QDateTime>
+#include <QTextBlock>
+#include <QMutex>
+static QMutex mutex;
+
+#define LOGFILENAME "C:\\HHShare.log"
+
+
+void customerMessageHandle(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    //Q_UNUSED(context)
+    QDateTime _datatime = QDateTime::currentDateTime();
+    QString szDate = _datatime.toString();
+    QString txtMessage(szDate);
+
+    switch (type)
+    {
+        case QtInfoMsg:
+            txtMessage += QString("[Info]");
+            break;
+        case QtDebugMsg:
+            txtMessage += QString("[Debug]");
+            break;
+        case QtWarningMsg:
+            txtMessage += QString("[Warning]");
+           break;
+        case QtCriticalMsg:
+            txtMessage += QString("[Critical]");
+            break;
+        case QtFatalMsg:
+            txtMessage += QString("[Fatal]");
+            //abort();
+            break;
+        default:
+            txtMessage += QString("[Trace]");
+            //abort();
+            break;
+    }
+
+    txtMessage.append(QString("%1").arg(context.file));
+    txtMessage.append(QString("<%1>").arg(context.line));
+    txtMessage.append(msg);
+
+    mutex.lock();
+    QFile outFile(LOGFILENAME);
+    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+
+    QTextStream textStream(&outFile);
+    textStream << txtMessage  << endl;
+    outFile.close();
+    mutex.unlock();
+}
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    qInstallMessageHandler(customerMessageHandle);
+
+    qDebug("This is a debug message");
+    qWarning("This is a warning message");
+    qCritical("This is a critical message");
+    //qFatal("This is a fatal message");
+
     MainWindow w;
     w.show();
     return a.exec();
